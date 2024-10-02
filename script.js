b3e5d0aa9e45f8dc95e8aa3d50bd588d72a21b68
labywebowka25 <173664@stud.prz.edu.pl>
2024-10-02 10:00:40 +0000

    ex1 + started ex2 lab1

--- script.js
+++ script.js
@@ -2,8 +2,13 @@
   //TODO
   const button = document.getElementById("ex1_button");
   const content = document.getElementById("ex1_content");
-
+  const content2 = document.getElementById("ex2_content");
   button.addEventListener("click", () => {
     content.innerHTML = "0,1,2,3,4,5,6,7,8,9";
-  });
+  } )
+  const input = document.getElementById("ex2_text");
+  input.addEventListener("input", () => {
+    content2.innerText = "";
+    if(input.value.length != 9)
+  })
 })();
