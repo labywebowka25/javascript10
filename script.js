1d032a91a5d86ce404201ce38182c7462307fdf7
labywebowka25 <173664@stud.prz.edu.pl>
2024-10-02 09:35:50 +0000

    ex1 done

--- script.js
+++ script.js
@@ -3,5 +3,7 @@
   const button = document.getElementById("ex1_button");
   const content = document.getElementById("ex1_content");
 
-  button.addEventListener("click", () => )
+  button.addEventListener("click", () => {
+    content.innerHTML = "0,1,2,3,4,5,6,7,8,9";
+  });
 })();
