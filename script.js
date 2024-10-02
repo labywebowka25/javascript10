c2cdf570f00f67fc63618379ff6d6ba869b17bb0
labywebowka25 <173664@stud.prz.edu.pl>
2024-10-02 09:31:38 +0000

    innit

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 (function () {
   //TODO
+  const button = document.getElementById("ex1_button");
+  const content = document.getElementById("ex1_content");
+
+  button.addEventListener("click", () => )
 })();
